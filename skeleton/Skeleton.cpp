2591c02e6d5282d5586283117ebf74274aa4356e
Gautam Mekkat <Blue9@users.noreply.github.com>
2019-11-15 23:45:59 -0500

    Inline first call in every basic block

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,21 +1,40 @@
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+
 using namespace llvm;
 
 namespace {
-  struct SkeletonPass : public FunctionPass {
-    static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+struct SkeletonPass : public FunctionPass {
+  static char ID;
+  SkeletonPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
-      return false;
+  virtual bool runOnFunction(Function &F) {
+    for (auto &B : F) {
+      for (auto &I : B) {
+        errs() << "<> Instruction: ";
+        I.print(llvm::errs());
+        CallInst *call = dyn_cast<CallInst>(&I);
+        if (call != nullptr) {
+          errs() << "\t\t<----- detected call instruction.\n";
+          InlineFunctionInfo info;
+          if (InlineFunction(call, info))
+            errs() << "<> INLINE SUCCESSFUL B)\n";
+          else
+            errs() << "<> INLINE failed >:(\n";
+					break;
+        }
+				errs() << "\n";
+      }
     }
-  };
-}
+    return false;
+  }
+};
+}  // namespace
 
 char SkeletonPass::ID = 0;
 
